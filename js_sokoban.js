b83ee3e50f933c54b966f5023b27da567a017d64
k-aki <k-aki@654044d4-2a77-4b30-b7f4-c2205c471b07>
2009-03-20 05:17:20 +0000

    MazeUtil.maxElemをMazeUtil.maxElementへ改名。 MazeUtil.maxElementが正しい答えを返さない不具合を修正。 MazeShapeクラスを追加。迷路の3D形状データをmain関数から分離。 splitMazeLines関数を追加。改行区切りの迷路データを行文字列配列へ変換。 init()、updateMazeModel()、startRotation()、stopRotation()を追加。mainからコードを分離し、回転の開始・停止やデータの更新ができるようにした。
    git-svn-id: svn+ssh://sakura.greenwood.co.jp/home/k-aki/svnrep/k-aki/js_sokoban@34 654044d4-2a77-4b30-b7f4-c2205c471b07

--- js_sokoban.js
+++ js_sokoban.js
@@ -1,3 +1,19 @@
+MazeUtil = {
+    maxElement: function(arr, pred)
+    {
+	if(arr.length <= 0){
+	    return null;
+	}
+	var result = arr[0];
+	for(var i = 1; i < arr.length; ++i){
+	    if(pred(result, arr[i])){
+		result = arr[i];
+	    }
+	}
+	return result;
+    }
+};
+
 
 Cell = {
     OUTSIDE: 0,
@@ -5,44 +21,20 @@ Cell = {
     WALL: 2
 };
 
+function Maze(cells, player)
+{
+    this.cells = cells;
+    this.player = player;
 
-MazeUtil = {
-    maxElem: function(arr, pred)
-    {
-	if(arr.length <= 0){
-	    return null;
-	}
-	var result = arr[0];
-	for(var i = 1; i < result; ++i){
-	    if(pred(result, arr[i])){
-		result = arr[i];
-	    }
-	}
-	return result;
-    }
-
-};
-
-
-function Maze(){
-    this.initialize.apply(this, arguments);
+    this.width = MazeUtil.maxElement(cells, function(lhs, rhs){return lhs.length < rhs.length;}).length;
+    this.height = cells.length;
 }
 Maze.prototype = {
-    initialize: function(cells, player)
-    {
-	this.cells = cells;
-	this.player = player;
-
-	this.width = MazeUtil.maxElem(cells, function(lhs, rhs){return lhs.length < rhs.length;}).length;
-	this.height = cells.length;
-    },
-
     cellAt: function(x, y)
     {
 	return (y >= 0 && y < this.cells.length && x >= 0 && x < this.cells[y].length)
 	    ? this.cells[y][x] : Cell.OUTSIDE;
-    },
-
+    }
 };
 
 
@@ -97,36 +89,13 @@ function outputMazeText(maze)
 
 
 
-var dataMazeTest =
-["...#####.......",
- "...#   #.......",
- ".###   #######.",
- ".#       @   #.",
- ".## ## #   ###.",
- "..# #      #...",
- "..# ####   #...",
- "..#        #...",
- "..##########..."];
-
-
-function main()
+function MazeShape(maze)
 {
-    var canvas = document.getElementById("canvas");
-    if(!canvas.getContext){
-	alert("Undefined canvas.getContext.");
-        return;
-    }
-    canvas.style.cssText = "border: 1px solid;"
-    var ctx = canvas.getContext("2d");
-
-    var maze = loadMaze(dataMazeTest);
-    //outputMazeText(maze);
-
     var WALL_HEIGHT = 1.0;
 
-    var vertices = new Array();
-    var transformed = new Array();
-    var surfaces = new Array();
+    this.vertices = new Array();
+    this.transformed = new Array();
+    this.surfaces = new Array();
 
     var imWall = new Image();
     imWall.src = "img/brick.png";
@@ -138,51 +107,51 @@ function main()
 	for(var x = 0; x < maze.width; ++x){
 	    switch(maze.cellAt(x, y)){
 	    case Cell.WALL:
-		idx = vertices.length/3;
-		vertices.push(x, WALL_HEIGHT, -y);
-		vertices.push(x, WALL_HEIGHT, -y-1);
-		vertices.push(x+1, WALL_HEIGHT, -y-1);
-		vertices.push(x+1, WALL_HEIGHT, -y);
-		vertices.push(x, 0, -y);
-		vertices.push(x, 0, -y-1);
-		vertices.push(x+1, 0, -y-1);
-		vertices.push(x+1, 0, -y);
+		idx = this.vertices.length/3;
+		this.vertices.push(x, WALL_HEIGHT, -y);
+		this.vertices.push(x, WALL_HEIGHT, -y-1);
+		this.vertices.push(x+1, WALL_HEIGHT, -y-1);
+		this.vertices.push(x+1, WALL_HEIGHT, -y);
+		this.vertices.push(x, 0, -y);
+		this.vertices.push(x, 0, -y-1);
+		this.vertices.push(x+1, 0, -y-1);
+		this.vertices.push(x+1, 0, -y);
 
-		surfaces.push(new Surface(
-		    transformed,
+		this.surfaces.push(new Surface(
+		    this.transformed,
 		    [idx+0, idx+1, idx+2, idx+3],
 		    [0,0, 0,1, 1,1, 1,0],
 		    imWall, "#000"));
-		surfaces.push(new Surface(
-		    transformed,
+		this.surfaces.push(new Surface(
+		    this.transformed,
 		    [idx+0, idx+4, idx+5, idx+1],
 		    [0,0, 0,1, 1,1, 1,0],
 		    imWall, "#000"));
-		surfaces.push(new Surface(
-		    transformed,
+		this.surfaces.push(new Surface(
+		    this.transformed,
 		    [idx+1, idx+5, idx+6, idx+2],
 		    [0,0, 0,1, 1,1, 1,0],
 		    imWall, "#000"));
-		surfaces.push(new Surface(
-		    transformed,
+		this.surfaces.push(new Surface(
+		    this.transformed,
 		    [idx+2, idx+6, idx+7, idx+3],
 		    [0,0, 0,1, 1,1, 1,0],
 		    imWall, "#000"));
-		surfaces.push(new Surface(
-		    transformed,
+		this.surfaces.push(new Surface(
+		    this.transformed,
 		    [idx+3, idx+7, idx+4, idx+0],
 		    [0,0, 0,1, 1,1, 1,0],
 		    imWall, "#000"));
 		break;
 	    case Cell.FLOOR:
-		idx = vertices.length/3;
-		vertices.push(x, 0, -y);
-		vertices.push(x, 0, -y-1);
-		vertices.push(x+1, 0, -y-1);
-		vertices.push(x+1, 0, -y);
+		idx = this.vertices.length/3;
+		this.vertices.push(x, 0, -y);
+		this.vertices.push(x, 0, -y-1);
+		this.vertices.push(x+1, 0, -y-1);
+		this.vertices.push(x+1, 0, -y);
 
-		surfaces.push(new Surface(
-		    transformed,
+		this.surfaces.push(new Surface(
+		    this.transformed,
 		    [idx+0, idx+1, idx+2, idx+3],
 		    [0,0, 0,1, 1,1, 1,0],
 		    imFloor, "#000"));
@@ -190,67 +159,142 @@ function main()
 	    }
         }
     }
+}
 
-    var angle = Math.PI/2;
 
+
+function init()
+{
+    timerRotation = null;
+    cameraAngle = Math.PI*0.4;
+    maze = null;
+    mazeShape = null;
+}
+
+function splitMazeLines(mazeText)
+{
+    var arrayLines = new Array();
+    arrayLines.push("");
+    for(var i = 0; i < mazeText.length; ++i){
+	var ch = mazeText.charAt(i);
+	if(ch == '\n'){
+	    arrayLines.push("");
+	}
+	else{
+	    arrayLines[arrayLines.length-1] += ch;
+	}
+    }
+    return arrayLines;
+}
+
+function updateMazeModel(arrayLines)
+{
+    maze = loadMaze(arrayLines);
+    //outputMazeText(maze); return;
+
+    mazeShape = new MazeShape(maze);
+
+    drawMazeShape();
+}
+
+
+function startRotation()
+{
+    if(timerRotation){
+	return;
+    }
     var onTime = function()
     {
-	var centerX = maze.width/2;
-	var centerY = maze.height/2;
-	var eyeX = centerX + Math.cos(angle) * 20;
-	var eyeY = centerY + Math.sin(angle) * 20;
-
-	angle += Math.PI/180*5;
-
-	var matView = Mat44.newLookAtLH(new Vec3(eyeX,20,-eyeY), new Vec3(centerX,0,-centerY), new Vec3(0,1,0));
-	var matProj = new Mat44(
-	    2/15,0,0,0,
-	    0,2/(15*canvas.height/canvas.width),0,0,
-	    0,0,1,0,
-	    0,0,0,1
-	);
-	var matScreen = new Mat44(
-	    canvas.width/2,0,0,0,
-	    0,-canvas.height/2,0,0,
-	    0,0,1,0,
-	    canvas.width/2,canvas.height/2,0,1);
-	var mat = new Mat44();
-	mat.mul(matView, matProj);
-	mat.mul(mat, matScreen);
-
-	// Transform vertices.
-	var vcount = vertices.length / 3;
-	var src = new Vec3();
-	var dst = new Vec3();
-	for(var vi = 0; vi < vcount; ++vi){
-	    src.set(vertices[vi*3+0], vertices[vi*3+1], vertices[vi*3+2]);
-	    dst.mul(src, mat);
-	    transformed[vi*3+0] = dst[0];
-	    transformed[vi*3+1] = dst[1];
-	    transformed[vi*3+2] = dst[2];
-	}
-
-	// Cull backfacing surfaces.
-	var frontFaces = new Array();
-	for(var si = 0; si < surfaces.length; ++si){
-	    if(surfaces[si].isFrontFace()){
-		frontFaces.push(surfaces[si]);
-	    }
-	}
-
-	// Sort surfaces by z.
-	frontFaces.sort(function(lhs, rhs) { return rhs.getMostFrontZ() - lhs.getMostFrontZ();});
-
-	// Draw surfaces.
-	ctx.fillStyle = "#cca";
-	ctx.fillRect(0,0,640,480);
-	for(var si = 0; si < frontFaces.length; ++si){
-	    drawSurface(ctx, frontFaces[si]);
-	}
+	cameraAngle += Math.PI/180*5;
+	drawMazeShape();
     };
+    timerRotation = setInterval(onTime, 100);
+}
 
-    setInterval(onTime, 100);
-    //setTimeout(onTime, 100);
+function stopRotation()
+{
+    if(!timerRotation){
+	return;
+    }
+    clearInterval(timerRotation);
+    timerRotation = null;
+}
 
+
+function drawMazeShape()
+{
+    var canvas = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+
+    var centerX = maze.width/2;
+    var centerY = maze.height/2;
+    var eyeX = centerX + Math.cos(cameraAngle) * 20;
+    var eyeY = centerY + Math.sin(cameraAngle) * 20;
+
+    var widthCells = maze.width;
+    var matView = Mat44.newLookAtLH(new Vec3(eyeX,30,-eyeY), new Vec3(centerX,0,-centerY), new Vec3(0,1,0));
+    var matProj = new Mat44(
+	2/widthCells,0,0,0,
+	0,2/(widthCells*canvas.height/canvas.width),0,0,
+	0,0,1,0,
+	0,0,0,1
+    );
+    var matScreen = new Mat44(
+	canvas.width/2,0,0,0,
+	0,-canvas.height/2,0,0,
+	0,0,1,0,
+	canvas.width/2,canvas.height/2,0,1);
+    var mat = new Mat44();
+    mat.mul(matView, matProj);
+    mat.mul(mat, matScreen);
+
+    // Transform vertices.
+    var vcount = mazeShape.vertices.length / 3;
+    var src = new Vec3();
+    var dst = new Vec3();
+    for(var vi = 0; vi < vcount; ++vi){
+	src.set(mazeShape.vertices[vi*3+0], mazeShape.vertices[vi*3+1], mazeShape.vertices[vi*3+2]);
+	dst.mul(src, mat);
+	mazeShape.transformed[vi*3+0] = dst[0];
+	mazeShape.transformed[vi*3+1] = dst[1];
+	mazeShape.transformed[vi*3+2] = dst[2];
+    }
+
+    // Cull backfacing surfaces.
+    var frontFaces = new Array();
+    for(var si = 0; si < mazeShape.surfaces.length; ++si){
+	if(mazeShape.surfaces[si].isFrontFace()){
+	    frontFaces.push(mazeShape.surfaces[si]);
+	}
+    }
+
+    // Sort surfaces by z.
+    frontFaces.sort(function(lhs, rhs) { return rhs.getMostFrontZ() - lhs.getMostFrontZ();});
+
+    // Draw surfaces.
+    ctx.fillStyle = "#cca";
+    ctx.fillRect(0,0,640,480);
+    for(var si = 0; si < frontFaces.length; ++si){
+	drawSurface(ctx, frontFaces[si]);
+    }
+}
+
+
+var dataMazeTest =
+["...#####.......",
+ "...#   #.......",
+ ".###   #######.",
+ ".#       @   #.",
+ ".## ## #   ###.",
+ "..# #      #...",
+ "..# ####   #...",
+ "..#        #...",
+ "..##########..."];
+
+function main()
+{
+    init();
+    updateMazeModel(dataMazeTest);
+    startRotation();
 }
 
